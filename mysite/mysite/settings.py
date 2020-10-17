1988d9fb3e38610f9122332efc13452c1e908b56
Anurag <anrgrt@gmail.com>
2020-10-17 19:03:45 +0530

    fixed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
 
 LOGIN_REDIRECT_URL = '/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 import dj_database_url
