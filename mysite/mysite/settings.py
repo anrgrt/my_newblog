51556c7ea14094a01e0e4e40b16d1da2daa9395f
Anurag <anrgrt@gmail.com>
2020-10-17 19:06:15 +0530

    fixed2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,8 +132,3 @@ STATICFILES_DIRS = (
 LOGIN_REDIRECT_URL = '/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
